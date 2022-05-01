c6d1d7bf39cbe38ac0daa652772fc7ea5ee063bc
datagutten <datagutten@datagutten.net>
2022-05-01 15:53:11 +0200

    Pass entire config to lap_timing class

--- infoscreen.php
+++ infoscreen.php
@@ -18,7 +18,7 @@ $filters->register_filters($twig);
 
 if (!empty($_GET['decoder'])) {
     try {
-        $lap_timing = new lap_timing($config['db'], $_GET['decoder']);
+        $lap_timing = new lap_timing($config, $_GET['decoder']);
         echo $twig->render('table.twig', [
             'laps' => $lap_timing->stats($lap_timing->laps($config['infoscreen']['round_limit'])),
             'time'=>date('H:i:s'),
